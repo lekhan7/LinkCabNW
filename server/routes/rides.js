2b34ffd5308f1457b1937edf97037a45482ae98c
lekhan7 <ktkarumbaiah@gmail.com>
2026-03-17 21:46:44 +0530

    lek

--- server/routes/rides.js
+++ server/routes/rides.js
@@ -8,6 +8,7 @@ const authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
 
+  
   if (!token) {
     return res.status(401).json({
       success: false,
